60c24e102557ba225950e2aa8bda589a61c2cbf7
jasanijahanvi@gmail.com <jasanijahanvi@gmail.com>
2022-04-26 18:37:34 +0530

    image size changed for mobile

--- js/index.js
+++ js/index.js
@@ -6,7 +6,12 @@
   var options = {
     type: 'carousel',
     perView: 1.3,
-    gap: 48
+    gap: 48,
+    breakpoints: {
+      1199: {
+        perView: 1
+      }
+    }
   }
   
   var glide = new Glide('.testimonials__slider', options).mount()

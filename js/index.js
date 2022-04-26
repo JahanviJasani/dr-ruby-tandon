1f0b51aa101e7f492b2bfeae289ca9ad37663c3d
jasanijahanvi@gmail.com <jasanijahanvi@gmail.com>
2022-04-26 15:34:14 +0530

    testimonials header implemented

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+(function() {
+  "use strict";
+
+  /* Testimonials Slider */
+  $('.testimonials__slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: '60px',
+  });
+  
+  
+
+})()

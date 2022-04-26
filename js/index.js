68c6ec5c3fc658ca3f0ba0d12e77e43b85959b63
jasanijahanvi@gmail.com <jasanijahanvi@gmail.com>
2022-04-26 22:31:36 +0530

    slider implemented - fully

--- js/index.js
+++ js/index.js
@@ -3,17 +3,10 @@
   "use strict";
 
   /* Testimonials Slider */
-  var options = {
-    type: 'carousel',
-    perView: 1.3,
-    gap: 48,
-    breakpoints: {
-      1199: {
-        perView: 1
-      }
-    }
-  }
-  
-  var glide = new Glide('.testimonials__slider', options).mount()
+  $('.testimonials__slider').slick({
+    dots: true,
+    appendDots: $('.tm--ind'),
+    arrows: false
+  });
   
 })()
